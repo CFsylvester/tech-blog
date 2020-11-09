aded89e86d49c72b772f336e6736e14ebf20938f
CFsylvester <clairesylvstr@gmail.com>
2020-11-09 17:46:41 -0500

    homepage shows all posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,48 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 //Main Homepage Route
 router.get('/', (req, res) => {
-    //found in views/homepage.handlebars
-    res.render('homepage');
+    Post.findAll({
+        order: [['created_at', 'DESC']], 
+        attributes: [
+            'id',
+            'post_url', 
+            'title', 
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+            model: User,
+            attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            //serialize the entry array
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            // pass a single post object into the homepage template
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
