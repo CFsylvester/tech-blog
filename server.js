c3c68b2118e5d67b13d0a0eb7fdd3cd61ec649db
CFsylvester <clairesylvstr@gmail.com>
2020-11-12 14:16:32 -0500

    helpers and partials

--- server.js
+++ server.js
@@ -1,50 +1,12 @@
-// // Dependencies
-// // =============================================================
-// const express = require('express');
-// const exphbs = require('express-handlebars');
-// // Requires the 'express-session' module
-// const session = require(`express-session`);
 
-// // Sets up the Express App
-// // =============================================================
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// // Sets up Sequelize
-// // =============================================================
-// const sequelize = require("./config/connection");
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// // Sets Handlebars as the default template engine
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
-
-// // Sets up the sessions with the 'secret', 'resave', 'saveUninitialized' options
-// app.use(
-//     session({
-//         secret: 'Super secret secret',
-//         cookie: {},
-//         resave: false,
-//         saveUninitialized: true,
-//         store: new SequelizeStore({
-//             db: sequelize
-//         })
-//     })
-// );
-
-// // Starts the server to begin listening
-// // =============================================================
-// app.listen(PORT, () => {
-//     console.log('App listening on PORT ' + PORT);
-// });
-  
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 //set up express-handlebars
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 //set up sessions
 const session = require('express-session');
 
