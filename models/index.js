8dd6b3e84f3900051db2e20f99b5eaeda4bb2cbe
Claire Sylvester <clairesylvstr@gmail.com>
2020-12-23 16:22:33 -0500

    new relic add on

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+require('newrelic');
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
